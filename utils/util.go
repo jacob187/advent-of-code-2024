078315f425afd7de2e967fa76e8bd617edcfca5f
Jacob Cohen <79770350+jacob187@users.noreply.github.com>
2024-12-06 11:07:40 -0500

    Refactor file parsing functions and add new utility to parse file content as a string

--- utils/util.go
+++ utils/util.go
@@ -7,19 +7,39 @@ import (
 	"strconv"
 )
 
-func ParseFileToStringArray(filePath string) []string {
-	var out []string
-
+func scanData(filePath string) (*bufio.Scanner, *os.File) {
 	data, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer data.Close()
 
-	scanner := bufio.NewScanner(data)
+	return bufio.NewScanner(data), data
+}
+func ParseFileToStringArray(filePath string) []string {
+
+	var out []string
+	scanner, file := scanData(filePath)
+	defer file.Close()
 
 	for scanner.Scan() {
 		out = append(out, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return out
+}
+
+func ParseFileToString(filePath string) string {
+
+	var out string
+	scanner, file := scanData(filePath)
+	defer file.Close()
+
+	for scanner.Scan() {
+		out += scanner.Text()
 	}
 
 	if err := scanner.Err(); err != nil {
